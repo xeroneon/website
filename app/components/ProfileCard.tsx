3d25d445533cb450b4c7b0ce2d5be3a2c9dfb33a
Xeroneon <andrewvasquez0113@gmail.com>
2024-08-27 20:04:58 -0700

    initial commit

---
+++ app/components/ProfileCard.tsx
@@ -0,0 +1,23 @@
+import { PiGithubLogo, PiLinkedinLogo, PiEnvelope } from "react-icons/pi";
+import { Button } from "./ui/button";
+export default function ProfileCard() {
+  return (
+    <div className="flex items-center w-full h-[200px] bg-slate-950">
+      <div className="container mx-auto flex flex-1 flex-col px-4">
+        <h1 className={`font-[Turkey] text-white mb-4 text-4xl`}>Xeroneon</h1>
+        <h4 className={`text-slate-400 text-xl mb-6`}>Software Engineer</h4>
+        <div className="flex space-x-4">
+          <Button className="bg-slate-50 text-black" size="icon">
+            <PiGithubLogo size={25} />
+          </Button>
+          <Button className="bg-slate-50 text-black" size="icon">
+            <PiLinkedinLogo size={25} />
+          </Button>
+          <Button className="bg-slate-50 text-black" size="icon">
+            <PiEnvelope size={25} />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/root.tsx
@@ -0,0 +1,51 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+} from "@remix-run/react";
+import "~/tailwind.css";
+import bg from "~/images/bg.png";
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { themeSessionResolver } from "./sessions.server";
+import { Theme, ThemeProvider, useTheme } from "remix-themes";
+import clsx from "clsx";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { getTheme } = await themeSessionResolver(request);
+  return {
+    theme: getTheme() || ("dark" as Theme),
+  };
+}
+
+export default function App() {
+  const data = useLoaderData<typeof loader>();
+  return (
+    <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+      <Document>
+        <Outlet />
+      </Document>
+    </ThemeProvider>
+  );
+}
+
+function Document({ children }: { children: React.ReactNode }) {
+  const [theme] = useTheme();
+  return (
+    <html lang="en" className={clsx(theme)}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="bg-slate-900 text-slate-50">
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
